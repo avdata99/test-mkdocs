b36bcf14850a7e5a9dc04cecb9f64404abb61cbf
avdata99 <andres@data99.com.ar>
2022-11-23 16:21:11 -0300

    Add more commands and test locally

--- code/okf-collab.py
+++ code/okf-collab.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from mkdocs.commands import build
+from mkdocs import config as mkdocs_config
 import click
 import yaml
 from helpers import get_lang_setting, get_list_setting
@@ -14,8 +16,11 @@ def cli():
     pass
 
 
-@cli.command()
-def build():
+@cli.command(
+    'build-config',
+    short_help='Build config files for all languages'
+)
+def build_config():
     """ Build the config file """
     base_config = yaml.safe_load(open(BASE_CONFIG_FOLDER / BASE_CONFIG_FILE))
     custom_config = yaml.safe_load(open(BASE_CONFIG_FOLDER / CUSTOM_CONFIG_FILE))
@@ -42,17 +47,61 @@ def build():
         config['site_author'] = get_lang_setting(custom_config, language, 'site_author')
         config['nav'] = custom_config['nav'][f'nav-{language}']
 
+        config['docs_dir'] = f"../page/docs/docs-{language}"
+        if language == 'en':
+            config['site_dir'] = f"../site"
+        else:
+            config['site_dir'] = f"../site/{language}"
+
+        wpdf_plugin['cover_title'] = config['site_name']
+        wpdf_plugin['cover_subtitle'] = config['site_description']
+        wpdf_plugin['author'] = config['site_author']
+
         # TODO
-        # extra - alternate
-        # extra assets_folder -> use github URL?
-        # Allow custom PDF settings (cover_title, cover_subtitle, etc)
-        # run `okf-collab build` in gh-action
+        # update extra - alternate with all languages
 
         # write the final config file
         final_config_file = BASE_CONFIG_FOLDER / f'mkdocs-{language}.yml'
         with open(final_config_file, 'w') as f:
             yaml.dump(config, f)
         click.echo(f'Config file written to {final_config_file}')
+
+@cli.command(
+    'build-site',
+    short_help='Build static site and PDFs for all languages'
+)
+def build_site():
+    """ Build the site """
+    custom_config = yaml.safe_load(open(BASE_CONFIG_FOLDER / CUSTOM_CONFIG_FILE))
+    languages = custom_config['site_name'].keys()
+
+    c = 0
+    for language in languages:
+        config_file = BASE_CONFIG_FOLDER / f'mkdocs-{language}.yml'
+        c += 1
+        dirty = c>1
+        click.echo(f'Building site for {language} (dirty:{dirty})')
+        build.build(mkdocs_config.load_config(config_file=open(config_file)), dirty=dirty)
+
+
+@cli.command(
+    'serve',
+    short_help='Serve static site'
+)
+def serve():
+    import http.server
+    import socketserver
+
+    PORT = 8033
+    DIRECTORY = "site"
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"serving at http://localhost:{PORT}")
+        httpd.serve_forever()
 
 
 if __name__ == '__main__':
